0be5c87eac4fbd5949e28fd12f532337e2823b17
Adam Carter <adm.carter@gmail.com>
2020-09-04 23:35:47 -0400

    Correct error in train_val_test_split in utilities_build.py

--- my_utilities/utilities_build.py
+++ my_utilities/utilities_build.py
@@ -21,7 +21,7 @@ def train_val_test_spit(X, y, test_size):
     :param y: an instance of :class: pd.Series
     :param test_size: a float, where 0 <= test_size < 1
     '''
-    X_train, X_test, y_train, y_test = train_test_split(x, y,
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
                 test_size=test_size, random_state=42)
     X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,
                 test_size=test_size, random_state=42)
